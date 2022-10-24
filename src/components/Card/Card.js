fe0ab5f9aa23a476cee301f221456aba454a5d40
Khaled <khaledkelany@gmail.com>
2022-10-24 15:01:26 +0300

    hour & 51min done continue later

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import './Card.css'
-import popular_01 from '../../assets/images/popular-01.jpg'
+
 
 
 const Card = (props) => {
   return (
     <div className='most-popular-item'>
     <div className='card-wrapper'>
-        <img className='most-popular-item-image' alt='' src={popular_01} />
+        <img className='most-popular-item-image' alt='' src={props.image} />
         <div className='most-popular-item-content'>
             <h4 className='most-popular-item-title'>
                 {props.title} <br/>
-                <span>Sandbox</span>
+                <span>{props.category}</span>
             </h4>
             <ul>
-                <li><span>4.8</span></li>
-                <li><span>2.3M</span></li>
+                <li><span>{props.rate}</span></li>
+                <li><span>{props.download}</span></li>
             </ul>
         </div>
     </div>
--- src/sections/MostPopular/MostPopular.js
+++ src/sections/MostPopular/MostPopular.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './MostPopular.css'
 import {Card} from '../../components/index'
+import popular_01 from '../../assets/images/popular-01.jpg'
+import popular_02 from '../../assets/images/popular-02.jpg'
+import popular_03 from '../../assets/images/popular-03.jpg'
+import popular_04 from '../../assets/images/popular-04.jpg'
+import popular_05 from '../../assets/images/popular-05.jpg'
+import popular_06 from '../../assets/images/popular-06.jpg'
 
 const MostPopular = () => {
   return (
@@ -10,10 +16,14 @@ const MostPopular = () => {
                 <h4>Most Popular</h4>
             </div>
             <div className='most-popular-items'>
-                <Card title="Fortnite"/>
-                <Card title="PubG"/>
-                <Card title="Dota2"/>
-                <Card title="CS-Go"/>
+                <Card image={popular_01} title="Fortnite" category="Sandbox"   rate="3.8"  download="2.3M"/>
+                <Card image={popular_02} title="PubG"     category="Steam-X"   rate="4.8"  download="1.1M"/>
+                <Card image={popular_03} title="Dota2"    category="Legandary" rate="2.7"  download="5.3M"/>
+                <Card image={popular_04} title="CS-Go"    category="Battle S"  rate="3.9"  download="6.1M"/>
+                <Card image={popular_05} title="Fortnite" category="Sandbox"   rate="3.8"  download="2.3M"/>
+                <Card image={popular_06} title="PubG"     category="Steam-X"   rate="4.8"  download="1.1M"/>
+                <Card image={popular_03} title="Dota2"    category="Legandary" rate="2.7"  download="5.3M"/>
+                <Card image={popular_02} title="CS-Go"    category="Battle S"  rate="3.9"  download="6.1M"/>
             </div>
             </div>
     </>
